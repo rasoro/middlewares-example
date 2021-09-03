d5fa13e6d15aa42f4d572a1f8fc5636ca6686901
Rafael Soares <rafael.dvlpr@gmail.com>
2021-09-03 08:39:50 -0300

    fead: add basic server example

--- standard/middleware.go
+++ standard/middleware.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("{\"message\": \"noice\"}\n"))
+		if err != nil {
+			fmt.Println("error handling main", err.Error())
+		}
+	})
+
+	fmt.Println("server listen at :3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
