2831741f46df8081e7241be426a7066718a47580
Rafael Soares <rafael.dvlpr@gmail.com>
2021-09-06 21:27:31 -0300

    feat: move healthcheck to own handler

--- standard/middleware.go
+++ standard/middleware.go
@@ -13,16 +13,18 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("{\"message\":\"ok\"}\n"))
+	if err != nil {
+		fmt.Println("error handling healthcheck", err.Error())
+	}
+}
+
 func main() {
 	http.HandleFunc("/", handleMain)
 
-	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-		_, err := w.Write([]byte("{\"message\":\"ok\"}\n"))
-		if err != nil {
-			fmt.Println("error handling healthcheck", err.Error())
-		}
-	})
+	http.HandleFunc("/healthcheck", handleHealthCheck)
 
-	fmt.Println("server listen at :3000")
-	log.Fatal(http.ListenAndServe(":3000", nil))
+	fmt.Println("server listen at :8090")
+	log.Fatal(http.ListenAndServe(":8090", nil))
 }
