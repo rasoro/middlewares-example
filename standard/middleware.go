cadae030f81486bfb65207cd6321b0b59d9f23f4
Rafael Soares <rafael.dvlpr@gmail.com>
2021-09-06 21:41:58 -0300

    feat: add response in json middleware

--- standard/middleware.go
+++ standard/middleware.go
@@ -10,6 +10,7 @@ import (
 func handleMain(w http.ResponseWriter, r *http.Request) {
 	res := map[string]string{"message": "noice"}
 	resJSON, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
 	_, err := w.Write(resJSON)
 	if err != nil {
 		fmt.Println("error handling main", err.Error())
@@ -19,16 +20,24 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 func handleHealthCheck(w http.ResponseWriter, r *http.Request) {
 	res := map[string]string{"message": "ok"}
 	resJson, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
 	_, err := w.Write(resJson)
 	if err != nil {
 		fmt.Println("error handling healthcheck", err.Error())
 	}
 }
 
-func main() {
-	http.HandleFunc("/", handleMain)
+func applicationJSON(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		h.ServeHTTP(w, r)
+	}
+}
 
-	http.HandleFunc("/healthcheck", handleHealthCheck)
+func main() {
+	http.HandleFunc("/", applicationJSON(handleMain))
+
+	http.HandleFunc("/healthcheck", applicationJSON(handleHealthCheck))
 
 	fmt.Println("server listen at :8090")
 	log.Fatal(http.ListenAndServe(":8090", nil))
