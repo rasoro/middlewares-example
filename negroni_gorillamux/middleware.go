3b9f970f64810c1d3db50a34203d1dc04c0296aa
Rafael Soares <rafael.dvlpr@gmail.com>
2021-09-09 22:29:52 -0300

    move handle main and handle healthcheck to own functions

--- negroni_gorillamux/middleware.go
+++ negroni_gorillamux/middleware.go
@@ -9,31 +9,35 @@ import (
 	"github.com/urfave/negroni"
 )
 
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	res := map[string]string{"message": "noice"}
+	resJSON, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write(resJSON)
+	if err != nil {
+		fmt.Printf("error in handleMain: %s", err.Error())
+	}
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request) {
+	res := map[string]string{"message": "ok"}
+	resJson, _ := json.Marshal(res)
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write(resJson)
+	if err != nil {
+		fmt.Printf("error in handleMain: %s", err.Error())
+	}
+}
+
 func main() {
 	n := negroni.Classic()
 
 	r := mux.NewRouter().StrictSlash(true)
 	n.UseHandler(r)
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		res := map[string]string{"message": "noice"}
-		resJSON, _ := json.Marshal(res)
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write(resJSON)
-		if err != nil {
-			fmt.Printf("error in handleMain: %s", err.Error())
-		}
-	}).Methods("GET")
+	r.HandleFunc("/", handleMain).Methods("GET")
 
-	r.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-		res := map[string]string{"message": "ok"}
-		resJson, _ := json.Marshal(res)
-		w.WriteHeader(http.StatusOK)
-		_, err := w.Write(resJson)
-		if err != nil {
-			fmt.Printf("error in handleMain: %s", err.Error())
-		}
-	}).Methods("GET")
+	r.HandleFunc("/healthcheck", handleHealth).Methods("GET")
 
 	fmt.Println("server listen at 8090")
 	err := http.ListenAndServe(":8090", n)
