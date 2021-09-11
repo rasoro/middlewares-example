a0d6567a7650ae2b7013b40e6dd51ca95a03ca9a
Rafael Soares <rafael.dvlpr@gmail.com>
2021-09-11 10:19:42 -0300

    feat: add applicationJson middleware to echo example

--- echo/middleware.go
+++ echo/middleware.go
@@ -4,10 +4,22 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+func applicationJSON(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		w := c.Response()
+		w.Header().Set("Content-Type", "application/json")
+		return next(c)
+	}
+}
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(applicationJSON)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, `{"message": "noice"}`)
